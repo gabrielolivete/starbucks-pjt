ee7ec71ba468ca9bbec1362c61bb3c6714d7933f
Gabriel Olivete <115937639+gabrielolivete@users.noreply.github.com>
2026-05-25 12:32:48 -0300

    Update project
    Frontend study project based on a tutorial - Starbucks clone

--- scripts.js
+++ scripts.js
@@ -7,3 +7,23 @@ mobileBtn.addEventListener('click', () => {
     icon.classList.toggle('fa-times');
     icon.classList.toggle('fa-bars');
 })
+
+const links = document.querySelectorAll('#nav-links a');
+
+links.forEach(link => {
+    link.addEventListener('click', () => {
+        navLinks.classList.remove('show');
+        icon.classList.remove('fa-times');
+        icon.classList.add('fa-bars');
+    })
+})
+
+window.addEventListener('scroll', () => {
+    const header = document.getElementById('header');
+
+    if (window.scrollY > 0) {
+        header.style.boxShadow = '0 4px 10px rgba(0, 0, 0, 0.1)';
+    } else {
+        header.style.boxShadow = 'none'
+    }
+})
